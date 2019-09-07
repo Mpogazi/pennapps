9586b5d76acc6b0ae0eb3398ee9549f8ee426c49
Fabrice B. Mpogazi <Fabrice.Mpogazi@tufts.edu>
2019-09-07 15:06:18 +0300

    setup for the app

---
+++ src/app/components/visual/visual.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'visual-component',
+    templateUrl: './visual.component.html',
+    styleUrls: ['./visual.component.scss']
+})
+
+export class visualComponent {
+    public placeholder: String;
+}
